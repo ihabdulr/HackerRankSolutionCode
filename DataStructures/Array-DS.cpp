dec3d0109a1e64c75a5a0896cc4a951f6e1a7111
Iman Hakim <ihabdulr@buffalo.edu>
2018-06-04 21:57:58 -0400

    Condense problem description

--- DataStructures/Array-DS.cpp
+++ DataStructures/Array-DS.cpp
@@ -1,23 +1,8 @@
 /*
 
-PROBLEM 
+PROBLEM:
 
-An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  (you may also see it written as ).
-
-Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
-
-Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
-
-Input Format
-
-The first line contains an integer,  (the number of integers in ). 
-The second line contains  space-separated integers describing .
-
-Constraints
-
-Output Format
-
-Print all  integers in  in reverse order as a single line of space-separated integers.
+Given an array, A, of N integers, print each element in reverse order as a single line of space-separated integers.
 
 SCORE: 10/10 possible points
 
