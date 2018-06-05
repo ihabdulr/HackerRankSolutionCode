620a273a6e411b37a71a10f58ac07912e203bbd5
Iman Hakim <ihabdulr@buffalo.edu>
2018-06-04 22:26:46 -0400

    Insert a node at the head of a linked list

---
+++ DataStructures/InsertAtHeadLinkedList.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: 
+  Insert Node at the begining of a linked list
+  Initially head pointer argument could be NULL for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+return back the pointer to the head of the linked list in the below method.
+
+Score: 10/10
+*/
+
+Node* Insert(Node *head,int data)
+{
+    Node* newNode = new Node; 
+    newNode->data = data;
+    newNode->next = head;
+    
+    
+  // Complete this method
+    
+    return newNode;
+}
