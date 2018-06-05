0b775ea39bc096435902df541b51817fe8d3d6ca
Iman Hakim <ihabdulr@buffalo.edu>
2018-06-04 22:15:27 -0400

    Print the Elements of a Linked List

---
+++ DataStructures/PrintLinkedList.cpp
@@ -0,0 +1,16 @@
+/*
+
+Given a pointer to the head node of a linked list, print its elements in order, one element per line. If the head pointer is null (indicating the list is empty), don’t print anything.
+
+*/
+
+void Print(Node *head)
+{
+ 
+    if(head){
+        std::cout << head->data << std::endl;
+        Print(head->next);
+    }
+    
+    
+}
